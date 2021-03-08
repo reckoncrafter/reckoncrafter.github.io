51313d57ff510ebe1bfc5e200e2125b32bfcece1
Dawson <dawsfinklea@gmail.com>
2021-03-08 01:10:30 -0600

    Added project files

---
+++ sidenav.js
@@ -0,0 +1,12 @@
+    function openNav() {
+        document.getElementById("variableWidth").style.width = "250px";
+    }    
+    function closeNav() {
+        document.getElementById("variableWidth").style.width = "0";
+    }
+    function hideBurger(){
+        document.getElementById("burger").style.opacity = "0";
+    }
+    function showBurger(){
+        document.getElementById("burger").style.opacity = "1";
+    }
